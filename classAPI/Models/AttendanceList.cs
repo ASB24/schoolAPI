c2671d7e297eb9c00cffc7a316f4db223e2d8775
ASB24 <alexserafini2001@gmail.com>
2022-09-07 11:06:25 -0400

    create AttendanceList model attendance module

---
+++ classAPI/Models/AttendanceList.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace classAPI.Models
+{
+    public class AttendanceList
+    {
+        [Key]
+        public int AttendanceID { get; set; }
+
+        [Display(Name = "Attendance date"), Required(ErrorMessage = "Provide a valid for the attendance list date")]
+        public DateTime AttendanceDate { get; set; }
+
+        [Display(Name = "Is Present?"), Required(ErrorMessage = "Provide a valid boolean for whether the student was present or not")]
+        public bool Grade { get; set; }
+
+        [Display(Name = "Student's ID"), Required(ErrorMessage = "Provide a valid Student ID")]
+        public int StudentID { get; set; }
+        public Student Student { get; set; }
+    }
+}
