f9314300c0898d9e1da89f4ff948c2fba3cc2e38
ASB24 <alexserafini2001@gmail.com>
2022-09-07 14:06:27 -0400

    modify classContext to schoolContext.

---
+++ classAPI/Models/SchoolContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace classAPI.Models
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options) { }
+
+        public DbSet<Student> Students { get; set; } = null!;
+        public DbSet<Grade> Grades { get; set; } = null!;
+        public DbSet<Attendance> Attendances { get; set; } = null!;
+
+    }
+}
--- classAPI/Program.cs
+++ classAPI/Program.cs
@@ -1,6 +1,14 @@
+using classAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<SchoolContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("SchoolContext")
+        )
+    );
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
