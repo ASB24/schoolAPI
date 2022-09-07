71b1ebc504d07a7ee979dbf96435f05ddefc2239
ASB24 <alexserafini2001@gmail.com>
2022-09-07 11:44:11 -0400

    refactor DataAnnotation for better readability

--- classAPI/Models/Student.cs
+++ classAPI/Models/Student.cs
@@ -7,13 +7,19 @@ namespace classAPI.Models
         [Key, Display(Name = "Student's ID")]
         public int StudentID { get; set; }
 
-        [Display(Name = "Student's first name"), StringLength(128), Required(ErrorMessage = "Provide a first name for the student")]
+        [Display(Name = "Student's first name")]
+        [StringLength(128, ErrorMessage = "Student's first name must be shorter than 128 characters")]
+        [Required(ErrorMessage = "Provide a first name for the student")]
         public string Name { get; set; }
 
-        [Display(Name = "Student's last name"), StringLength(128), Required(ErrorMessage = "Provide a last name for the student")]
+        [Display(Name = "Student's last name")]
+        [StringLength(128, ErrorMessage = "Student's last name must be shorter than 128 characters")]
+        [Required(ErrorMessage = "Provide a last name for the student")]
         public string LastName { get; set; }
 
-        [Display(Name = "Student's class"), StringLength(5), Required(ErrorMessage = "Provide a class for the student")]
+        [Display(Name = "Student's class")]
+        [StringLength(5, ErrorMessage = "Class name must be shorter than 5 characters")]
+        [Required(ErrorMessage = "Provide a class for the student")]
         public string Class { get; set; }
 
         [Display(Name = "Student's addition date")]
