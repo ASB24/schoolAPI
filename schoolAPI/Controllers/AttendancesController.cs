3d1da9c9338aaabe8cde1c317d816df58941c2cf
ASB24 <alexserafini2001@gmail.com>
2022-09-08 14:53:53 -0400

    create Attendances and Grades controllers

---
+++ schoolAPI/Controllers/AttendancesController.cs
@@ -0,0 +1,109 @@
+﻿using schoolAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace schoolAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AttendancesController : Controller
+    {
+        private readonly SchoolContext schoolContext;
+        private bool AttendanceExists(int id) { return (schoolContext.Attendances?.Any(s => s.ID == id)).GetValueOrDefault(); }
+
+        public AttendancesController(SchoolContext schoolContext)
+        {
+            this.schoolContext = schoolContext;
+        }
+
+        //GET: api/Attendances
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Attendance>>> GetAttendances()
+        {
+            if (schoolContext.Attendances == null)
+            {
+                return NotFound();
+            }
+
+            return await schoolContext.Attendances.ToListAsync();
+        }
+
+        //GET: api/Attendances/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Attendance>> GetAttendance(int id)
+        {
+            if (schoolContext.Attendances == null)
+            {
+                return NotFound();
+            }
+
+            var attendance = await schoolContext.Attendances.FindAsync(id);
+            if (attendance == null) return NotFound();
+
+
+
+            return attendance;
+        }
+
+        //POST: api/Attendances
+        [HttpPost]
+        public async Task<ActionResult<Attendance>> PostAttendance(Attendance attendance)
+        {
+            var student = await schoolContext.Students.FindAsync(attendance.StudentID);
+            if (student == null) return NotFound("Student with provided Student ID not found");
+
+            attendance.CreatedAt = DateTime.Now;
+            attendance.LastUpdatedAt = DateTime.Now;
+
+            schoolContext.Attendances.Add(attendance);
+            await schoolContext.SaveChangesAsync();
+
+            return attendance;
+        }
+
+        //PUT: api/Attendances/{id}
+        [HttpPut]
+        public async Task<IActionResult> PutAttendance(Attendance attendance)
+        {
+            var student = schoolContext.Students.Find(attendance.StudentID);
+            if (student == null) return NotFound("Student with provided Student ID not found");
+
+            schoolContext.Entry(attendance).State = EntityState.Modified;
+
+            try
+            {
+                attendance.LastUpdatedAt = DateTime.Now;
+                await schoolContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                if (!AttendanceExists(attendance.ID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw ex;
+                }
+            }
+
+            return NoContent();
+        }
+
+        //DELETE: api/Attendances/{id}
+        [HttpDelete]
+        public async Task<IActionResult> DeleteAttendance(int id)
+        {
+            if (schoolContext.Attendances == null) return NotFound();
+
+            var attendance = await schoolContext.Attendances.FindAsync(id);
+            if (attendance == null) return NotFound();
+
+            schoolContext.Attendances.Remove(attendance);
+            await schoolContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
--- schoolAPI/Controllers/GradesController.cs
+++ schoolAPI/Controllers/GradesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using schoolAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace schoolAPI.Controllers
 {
@@ -6,6 +8,101 @@ namespace schoolAPI.Controllers
     [ApiController]
     public class GradesController : Controller
     {
-        
+        private readonly SchoolContext schoolContext;
+        private bool GradeExists(int id) { return (schoolContext.Grades?.Any(s => s.ID == id)).GetValueOrDefault(); }
+
+        public GradesController(SchoolContext schoolContext)
+        {
+            this.schoolContext = schoolContext;
+        }
+
+        //GET: api/Grades
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Grade>>> GetGrades()
+        {
+            if (schoolContext.Grades == null)
+            {
+                return NotFound();
+            }
+
+            return await schoolContext.Grades.ToListAsync();
+        }
+
+        //GET: api/Grades/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Grade>> GetGrade(int id)
+        {
+            if (schoolContext.Grades == null)
+            {
+                return NotFound();
+            }
+
+            var grade = await schoolContext.Grades.FindAsync(id);
+            if (grade == null) return NotFound();
+
+
+
+            return grade;
+        }
+
+        // TODO: rest of endpoints and endpoints for grades and attendance
+        //POST: api/Grades
+        [HttpPost]
+        public async Task<ActionResult<Grade>> PostGrade(Grade grade)
+        {
+            var student = await schoolContext.Students.FindAsync(grade.StudentID);
+            if (student == null) return NotFound("Student with provided Student ID not found");
+            
+            grade.CreatedAt = DateTime.Now;
+            grade.LastUpdatedAt = DateTime.Now;
+
+            schoolContext.Grades.Add(grade);
+            await schoolContext.SaveChangesAsync();
+
+            return grade;
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutGrade(Grade grade)
+        {
+            var student = await schoolContext.Students.FindAsync(grade.StudentID);
+            if (student == null) return NotFound("Student with provided Student ID not found");
+
+            schoolContext.Entry(grade).State = EntityState.Modified;
+
+            try
+            {
+                grade.LastUpdatedAt = DateTime.Now;
+                await schoolContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                if (!GradeExists(grade.ID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw ex;
+                }
+            }
+
+            return NoContent();
+        }
+
+        //DELETE: api/Grades/{id}
+        [HttpDelete]
+        public async Task<IActionResult> DeleteGrade(int id)
+        {
+            if (schoolContext.Grades == null) return NotFound();
+
+            var grade = await schoolContext.Grades.FindAsync(id);
+            if (grade == null) return NotFound();
+
+            schoolContext.Grades.Remove(grade);
+            await schoolContext.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
