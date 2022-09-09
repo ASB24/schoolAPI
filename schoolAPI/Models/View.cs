f9dc46844e93d9647853892bdcae8354053eac70
ASB24 <alexserafini2001@gmail.com>
2022-09-09 11:23:59 -0400

    create Views models

---
+++ schoolAPI/Models/View.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace schoolAPI.Models
+{
+    public class View
+    {
+        [Keyless]
+        public class GradeRow
+        {
+            public int StudentID { get; set; }
+            public string StudentName { get; set; }
+            public string StudentLastName { get; set; }
+            public int GradeID { get; set; }
+            public string Subject { get; set; }
+            public int Grade { get; set; }
+            public string Literal { get; set; }
+        }
+
+        [Keyless]
+        public class AttendanceRow
+        {
+            public int AttendanceID { get; set; }
+
+            [DataType(DataType.Date)]
+            [Column(TypeName = "Date")]
+            public DateTime AttendanceDate { get; set; }
+            public int StudentID { get; set; }
+            public string StudentName { get; set; }
+            public string StudentLastName { get; set; }
+            public bool IsPresent { get; set; }
+        }
+    }
+}
