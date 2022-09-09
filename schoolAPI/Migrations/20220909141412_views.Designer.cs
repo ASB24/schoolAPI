afaacb2ab9ae93779ac53697a0693418656a1ed1
ASB24 <alexserafini2001@gmail.com>
2022-09-09 11:23:39 -0400

    add views migration

--- schoolAPI/Migrations/20220909141412_views.Designer.cs
+++ schoolAPI/Migrations/20220909141412_views.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using schoolAPI.Models;
 
@@ -11,9 +12,10 @@ using schoolAPI.Models;
 namespace schoolAPI.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20220909141412_views")]
+    partial class views
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ schoolAPI/Migrations/20220909141412_views.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace schoolAPI.Migrations
+{
+    public partial class views : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("GO\r\n" +
+                "create view grades_view as\r\n" +
+                "select " +
+                "Students.ID as [StudentID], Students.Name as StudentName, Students.LastName as StudentLastName,\r\n\t   " +
+                "Grades.ID as [GradeID], Grades.Subject, Grades.GradeScore as [Grade], Grades.Literal\r\n" +
+                "from Grades\r\n" +
+                "inner join Students on Students.ID = Grades.StudentID\r\n\r\n" +
+                "GO\r\n\r\n" +
+                "create view attendances_view as\r\n" +
+                "select Attendances.ID as [AttendanceID], Attendances.AttendanceDate, " +
+                "Students.ID as [StudentID], \r\n\t   Students.Name as [StudentName], " +
+                "Students.LastName as [StudentLastName], Attendances.IsPresent\r\n" +
+                "from Attendances\r\n" +
+                "inner join Students on Students.ID = Attendances.StudentID\r\n\r\n" +
+                "GO");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("" +
+                "drop view grades_view\r\n" +
+                "drop view attendances_view");
+        }
+    }
+}
