9b49acd47c2a8d9cc9415addce374f97ed1229bb
ASB24 <alexserafini2001@gmail.com>
2022-09-12 02:42:44 -0400

    allow CORS for frontend

--- schoolAPI/Program.cs
+++ schoolAPI/Program.cs
@@ -14,6 +14,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "_allowSpecificOrigins",
+        policy => policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader()
+        );
+});
 
 var app = builder.Build();
 
@@ -25,6 +31,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("_allowSpecificOrigins");
 
 app.UseAuthorization();
 
